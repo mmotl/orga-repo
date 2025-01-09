065954f9247fac96c12fcb48a70cc9a2e85776a6
Matthias Motl <matthias@motl.de>
2025-01-09 17:32:03 +0100

    sql script

--- sample.sql
+++ sample.sql
@@ -1,3 +1,4 @@
+-- multi solutions test
 
 SELECT * FROM table;
 -- START_SOLUTION
@@ -5,3 +6,16 @@ DELETE FROM table WHERE id = 1;
 -- END_SOLUTION
 SELECT * FROM another_table;
 
+
+SELECT * FROM table;
+-- START_SOLUTION
+DELETE FROM table WHERE id = 1;
+-- END_SOLUTION
+SELECT * FROM another_table;
+
+
+SELECT * FROM table;
+-- START_SOLUTION
+DELETE FROM table WHERE id = 1;
+-- END_SOLUTION
+SELECT * FROM another_table;
