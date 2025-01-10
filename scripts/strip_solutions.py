3d123f8a24479b700200f70997c6ccd12a9668f0
Matthias Motl <matthias@motl.de>
2025-01-10 15:40:35 +0100

    add repo root function to scripts

--- scripts/strip_solutions.py
+++ scripts/strip_solutions.py
@@ -11,10 +11,22 @@ def strip_solution_cells(nb_path):
 
     with open(nb_path, 'w') as f:
         nbformat.write(nb, f)
+def find_repository_root():
+    """
+    Finds the root of the repository by searching for a .git folder.
+    """
+    current_dir = os.getcwd()
+    while current_dir != os.path.dirname(current_dir):  # Stop at filesystem root
+        if os.path.isdir(os.path.join(current_dir, '.git')):
+            return current_dir
+        current_dir = os.path.dirname(current_dir)
+    return os.getcwd()  # Default to current directory if no .git is found
+
+repository_root = find_repository_root()
 
 # Find all notebooks in the "notebooks/" directory (you can customize the folder path)
 #notebooks_directory = 'notebooks/'  # Replace with the directory where teachers save their notebooks
-notebooks = glob.glob(os.path.join(#notebooks_directory,
+notebooks = glob.glob(os.path.join(repository_root,
                                     '**/*.ipynb'), recursive=True)
 
 # Process each notebook found
